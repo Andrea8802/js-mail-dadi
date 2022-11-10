c3cf9adce9da47aeedf8868b10701a6f547da7a3
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-10 15:05:58 +0100

    Mail Project Done

---
+++ js-mail/js/main.js
@@ -0,0 +1,23 @@
+// Verificare che l'email inserita sia fra quelle all'interno della lista delle email ammesse.
+
+// Stiliamo una lista con un array delle email che possono accedere.
+const emailAutorizzate = ["giovanniaugello23@gmail.com", "lorenzoferrari009@yahoo.com", "mariastassi@libero.it"];
+
+// Input dell'email dell'utente.
+let emailUtente = prompt("Inserisci la tua email");
+let autorizzato;
+// Verifichiamo se l'email è autorizzata o meno.
+for (let i = 0; i < emailAutorizzate.length; i++){
+    if (emailUtente === emailAutorizzate[i]){
+        // Output dell'esito.
+        console.log("Benvenuto!");
+        autorizzato = 1;
+    }
+
+    if (autorizzato != 1){
+        // Output dell'esito.
+        console.log("Mi dispiace, ma la sua email non è autorizzata all'accesso!");
+    }
+}
+
+
